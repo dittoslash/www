fed4e6e6535018b9c16c9a450a8b9793bd926687
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-02 20:30:08 +0100

    st33l

--- lib/steel.php
+++ lib/steel.php
@@ -1,9 +1,8 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
-require "/var/www/secconf.php"
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-
+require "/var/www/secconf.php";
 ?>

c5b6004a3e85a5b99b184429c0e04ec88e9b37a2
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-02 20:27:51 +0100

    more connections

--- lib/steel.php
+++ lib/steel.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
+require "/var/www/secconf.php"
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
+
+
 ?>

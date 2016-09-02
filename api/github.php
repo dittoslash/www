700609f51b9f1ae2769335c288244c1f4dd23472
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-02 17:41:28 +0100

    i hate semicolons

--- api/github.php
+++ api/github.php
@@ -1,5 +1,5 @@
 <?php
-require "steel.php"
+require "steel.php";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 shell_exec( 'cd /var/www/html/ && git reset --hard HEAD && git pull' );
 ?>Github pull endpoint. You've seen nothing.
--- lib/steel.php
+++ lib/steel.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . "/vendor/autoload.php"
+require __DIR__ . "/vendor/autoload.php";
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();

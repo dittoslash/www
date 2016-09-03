288d7696fb8e8da1ac2cef0fbac78a4480ecaf27
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-03 08:15:58 +0100

    included medoo lib, next commit will be from the server with the composer install

--- lib/steel.php
+++ lib/steel.php
@@ -4,5 +4,13 @@ $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-require "/var/www/secconf.php";
+require "/var/www/secconf.php"; // defines $DB_DATABASE, $DB_HOST, $DB_PASS, and $DB_USER
+
+global $db = new medoo([
+  'database_type' => 'mysql'
+  'datbase_name' => $DB_DATABASE
+  'server' => $DB_HOST
+  'username' => $DB_USER
+  'password' => $DB_PASS
+]);
 ?>

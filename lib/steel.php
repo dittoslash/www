f5b61a7e4e88ccf58e86ca9042ade551cec4afc3
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-03 08:20:42 +0100

    meme

--- lib/steel.php
+++ lib/steel.php
@@ -7,10 +7,10 @@ $whoops->register();
 require "/var/www/secconf.php"; // defines $DB_DATABASE, $DB_HOST, $DB_PASS, and $DB_USER
 
 global $db = new medoo([
-  'database_type' => 'mysql'
-  'datbase_name' => $DB_DATABASE
-  'server' => $DB_HOST
-  'username' => $DB_USER
+  'database_type' => 'mysql',
+  'datbase_name' => $DB_DATABASE,
+  'server' => $DB_HOST,
+  'username' => $DB_USER,
   'password' => $DB_PASS
 ]);
 ?>

c2ec26df8aa74aee0567797a450f392511fa19d5
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-03 08:47:43 +0100

    added template

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php require "steel.php" ?>
+<?php require "steel.php"; require "template.php" ?>
 
 
 New site under development. Once I get libs and things down I'll develop this page a bit more.
-Doot troop.
 <a href="http://dittoslash.uk:30080"> Old site </a>
---
+++ lib/template.php
@@ -0,0 +1,4 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+</head>

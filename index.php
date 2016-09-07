d7ad0803262b726d1cd328923af74656da06f150
Dittoslash <dittoslash@users.noreply.github.com>
2016-09-07 07:41:11 +0100

    Bleh.

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php require "template.php" ?>
-
-
-I am bored & can't think of anything to write here. lux@dittoslash.uk to talk to me, queries@dittoslash.uk to ask about the site.
-<br/>
-<a href="http://dittoslash.uk:30080"> Old site </a>
